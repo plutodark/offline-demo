a6e1f545d3c58ba94168be074520d1d67b10c8f0
Paul Lai <plutodark@gmail.com>
2024-02-28 23:01:32 +0000

    offline demo

---
+++ public/service-worker.js
@@ -0,0 +1,43 @@
+var CACHE_NAME = 'offline-demo-cache';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/js/main.2b43367e.js',
+  '/static/css/main.f855e6bc.css',
+  '/favicon.ico',
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  var cacheWhitelist = ['offline-demo-cache'];
+
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { openDB } from 'idb';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const db = await openDB('offline-demo-db', 1, {
+        upgrade(db) {
+          db.createObjectStore('data-store', { autoIncrement: true, keyPath: 'id' });
+        },
+      });
+
+      const tx = db.transaction('data-store', 'readonly');
+      const store = tx.objectStore('data-store');
+      const items = await store.getAll();
+      setData(items);
+    }
+
+    fetchData();
+  }, []);
+
+  const handleAddData = async () => {
+    const newData = { name: `Item ${data.length + 1}` };
+
+    const db = await openDB('offline-demo-db', 1);
+    const tx = db.transaction('data-store', 'readwrite');
+    const store = tx.objectStore('data-store');
+    await store.add(newData);
+
+    setData([...data, newData]);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Offline Demo with IndexedDB</h1>
+        <button onClick={handleAddData}>Add Data</button>
+        <ul>
+          {data.map((item, index) => (
+            <li key={index}>{item.name}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
