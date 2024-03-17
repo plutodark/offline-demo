9c418433ab4a7649b5de0fa98a25a510dfc033ab
Paul Lai <plutodark@gmail.com>
2024-03-17 10:13:17 +0000

    indexed stress test

--- src/App.js
+++ src/App.js
@@ -1,44 +1,56 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
   // getItems,
   addItem,
   updateItem,
   deleteItem,
   bulkAddItems,
-  db
+  multiBulkAdd,
+  searchItems,
+  //getLastItem,
+  // db
 } from './db';
-import { useLiveQuery } from "dexie-react-hooks";
+// import { useLiveQuery } from "dexie-react-hooks";
 
 const ItemList = () => {
- // const [items, setItems] = useState([]);
   const [query, setQuery] = useState('');
+  const [seconds, setSeconds] = useState(0);
+  const [items, setItems] = useState([]);
   const offset = 0;
   const limit = 10;
-  const handleSearchQuery = () => {
-    console.log('query', query)
-    return db.items
-    .where('name')
-    .startsWith(query)
-    //.anyOf(query)
-    .offset(offset)
-    .limit(limit)
-    .sortBy('id')
-  }
-  const items = useLiveQuery(
-    handleSearchQuery,
-      [query],
-      []
-  );
-  console.log('items', items);
+  // const handleSearchQuery = () => {
+  //   const regex = new RegExp(query, 'i');
+  //   const collection = query ? db.items.filter(item => regex.test(item.name)) : db.items;
+    
+  //   return collection
+  //   .offset(offset)
+  //   .limit(limit)
+  //   .sortBy('id')
+  // }
+  // const items = useLiveQuery(
+  //   handleSearchQuery,
+  //     [query],
+  //     []
+  // );
+  useEffect(() => {
+    const fetchData = async () => {
+      const { result, consumedSeconds } = await searchItems({ offset, limit, query });
+      setItems(result);
+      setSeconds(consumedSeconds);
+    }
+    fetchData();
+  }, [query]);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const allItems = await getItems({ offset: 0, limit: 10 });
-  //     setItems(allItems);
-  //   };
+  useEffect(() => {
+    const fetchData = async () => {
+      // const item = await getLastItem();
+      // console.log(item);
+      // const count = item ? item.id : 1;
+      // console.log('count', count);
+    };
 
-  //   fetchData();
-  // }, []);
+    fetchData();
+  }, [items]);
 
   const handleAdd = (newItem) => {
     addItem(newItem);
@@ -59,6 +71,7 @@ const ItemList = () => {
     <div>
       <div>search</div>
       <input type="text" name="name" onChange={e => setQuery(e.target.value)} value={query} />
+      <div>It takes {seconds} seconds to finished the search!</div>
       <h2>My Items</h2>
       
       <ul>
@@ -82,6 +95,7 @@ const ItemList = () => {
         <button type="submit">Add Item</button>
       </form>
       <button onClick={bulkAddItems}>Bulk add</button>
+      <button onClick={multiBulkAdd}>Mulit bulk add</button>
     </div>
   );
 };
--- src/db.js
+++ src/db.js
@@ -14,7 +14,20 @@ export const getItems = async ({ offset = 0, query = '', limit = 10 }) => {
   const allItems = await db.items.where("name").startsWithAnyOfIgnoreCase([query]).offset(offset).limit(limit).toArray();
   return allItems;
 };
-
+export const searchItems = async({ offset = 0, query = '', limit = 10 }) => {
+  console.log('start searching');
+  const regex = new RegExp(query, 'i');
+  const collection = query ? db.items.filter(item => regex.test(item.name)) : db.items;
+  const startTime = performance.now();
+  
+  const result = await collection
+  .offset(offset)
+  .limit(limit)
+  .sortBy('id');
+  const endTime = performance.now();
+  const consumedSeconds = (endTime - startTime) / 1000;
+  return { result, consumedSeconds };
+}
 export const updateItem = async (id, updatedItem) => {
   await db.items.update(id, updatedItem);
 };
@@ -23,19 +36,33 @@ export const deleteItem = async (id) => {
   await db.items.delete(id);
 };
 
+export const getLastItem = async () => {
+  return await await db.items.orderBy('id').last();
+}
 export const bulkAddItems = async () => {
   // const numItems = 5000; // Adjust based on your test and browser limitations
   // const chunkSize = 1000000; // 1MB per item (example)
 console.log('start');
-const numItems = 100000;
+const totalNum = 100000;
   const items = [];
-  for (let i = 0; i < numItems; i++) {
-    const name = `a${i}bcdefghi`.substring(0, 10);
+  const lastItem = await getLastItem();
+  const lastCount = lastItem ? lastItem.id : 0;
+  const numItems = totalNum + lastCount;
+  for (let i = lastCount; i < numItems; i++) {
+    const name = `${i}abcdefghijklmnopqrstuv`.substring(0, 20);
     items.push({ name, purchased: false }); // Simulate data
   }
 
   const startTime = performance.now();
   await db.items.bulkAdd(items);
   const endTime = performance.now();
-  console.log(`Added ${numItems} items (Est. size: ${numItems * 10 / (1024 * 1024)} MB) in ${(endTime - startTime) / 1000} seconds`);
+  console.log(`Added ${numItems} items (Est. size: ${numItems * 20 / (1024 * 1024)} MB) in ${(endTime - startTime) / 1000} seconds`);
+}
+
+export const multiBulkAdd = async () => {
+  const times = 100;
+  for(let j = 0; j < times; j++) {
+    await bulkAddItems();
+    console.log('time', j);
+  }
 }
